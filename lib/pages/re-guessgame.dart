f4f86bd0bf802a3b7ece56dc934bafd0dc123bbf
Tommy <beetommy72@gmail.com>
2024-11-29 09:04:50 +0100

    a better approach

---
+++ lib/pages/re-guessgame.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import "dart:math";
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(GuessGame());
+}
+
+class GuessGame extends StatefulWidget {
+  const GuessGame({super.key});
+
+  @override
+  State<GuessGame> createState() => _GuessGameState();
+}
+
+class _GuessGameState extends State<GuessGame> {
+  TextEditingController userInput = TextEditingController();
+
+  int randomNumber = 0;
+  String congratMsg = "";
+
+  void generateRandomNumber() {
+    randomNumber = Random().nextInt(11);
+  }
+  void checkGuess() {
+    setState(() {
+      if (int.parse(userInput.text) == randomNumber) {
+        congratMsg = "You guessed the number right! The number was indeed $randomNumber";
+        generateRandomNumber();
+      } else{
+        congratMsg = "Failed to guess the correct number. Try again!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        appBar: AppBar(
+          title: Text(
+            "Model Of Guess Game",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Guess a number",
+                  style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFFFFFF)),
+                ),
+                TextField(
+                  controller: userInput,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Guess a number from 1 - 10..",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: checkGuess,
+                  child: Text("Guess"),
+                  style: ButtonStyle(),
+                ),
+                Text(congratMsg),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
